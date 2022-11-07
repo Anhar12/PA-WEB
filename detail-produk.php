5743e2f5e2d179b397a760113e92a6ad1ca05215
DH1YA <89460039+DH1YA@users.noreply.github.com>
2022-11-07 11:26:55 +0800

    detail produk
    penambahan bagian detail produk  dengan detail-produk.php sebagai rangkanya
    dan style-detail-produk.css untuk menghias

---
+++ detail-produk.php
@@ -0,0 +1,151 @@
+<?php
+session_start();
+require 'koneksi.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/AZ.png">
+    <title>AnharZtore</title>
+    <link rel="stylesheet" href="style-detail-produk.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!-- header -->
+    <div class="atas">
+        <nav>
+            <a href="index.php" id="logo"> Anhar <font color="#f86909"> Ztore </font> </a>
+            <div class="navbar">
+                <ul>
+                    <li> <a href="index.php"> HOME </a></li>
+                    <li> <a href="#product"> PRODUCT </a></li>
+                    <li> <a href="about.php"> ABOUT </a></li>
+                    <li> <a href="login.php"> LOGIN </a></li>
+                    <li>
+                        <label>
+                            <input type="checkbox" class="checkbox" id="tombol">
+                            <span class="check"></span>
+                        </label>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- main content -->
+        <?php
+      $id = $_GET['id'];
+
+      $result = mysqli_query($conn, "SELECT * FROM produk WHERE id=$id");
+      
+      $produk = [];
+
+      while ($row = mysqli_fetch_assoc($result)) {
+          $produk[] = $row;
+      }
+      foreach ($produk as $p)
+
+        ?>
+    <!-- <div class = "main-wrapper"> -->
+        <div class = "container">
+            <div class = "product-div">
+                <div class = "product-div-left">
+                    <div class = "img-container">
+                        <img src = "img/<?php echo $p['gambar'];?>" alt = "HP">
+                    </div>
+                </div>
+                <div class = "product-div-right">
+                    <span class = "product-name"><?php echo $p['nama'];?></span>
+                    <span class = "product-price">Rp <?php echo $p['harga'];?></span>
+                    <span class = "product-kategori">Kategori :  <?php echo $p['kategori'];?></span>
+                    <p class = "product-description"><?php echo $p['deskripsi'];?></p>
+                    <div class = "btn-groups">
+                        <button type = "button" class = "buy-now-btn"><i class = "fas fa-wallet"></i>buy now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- </div> -->
+
+            <!-- review -->
+            <div class="review">
+                <h1> Review Dari Para Customer Kami </h1>
+                <div class="baris">
+                    <div class="kolomReview">
+                        <img src="assets/rizky.jpg">
+                        <div>
+                            <p>
+                                Tempatnya trusted, adminnya fast respon, terbaeklah pokonya, never gonna give u up
+                            </p>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-half-o"></i>
+                            <h2> Rizky Slebew </h2>
+                        </div>
+                    </div>
+                    <div class="kolomReview">
+                        <img src="assets/rangga.jpg">
+                        <div>
+                            <p>
+                                Saya awalnya dapat ingfo dari andri, tempatnya trusted, sayangnya gaada gratis rokok
+                            </p>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                            <i class="fa fa-star-o"></i>
+                            <h2> Rangga Banyak tanya </h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="baris2Review">
+                    <div class="kolomReview">
+                        <img src="assets/terkadang.jpg">
+                        <div>
+                            <p>
+                                Awalnya saya coba coba cukur mullet, eh ternyata membuatku kepelet, bintang 5 deh
+                            </p>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <h2> Terkadang Mullet </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- footer -->
+            <footer>
+                <div class="footer">
+                    <p>
+                        Jangan lupa belanja di AnharZtore, serta follow akun ig saya <a href="https://www.instagram.com/anharrrrrr_/" id="ig"> @anharrrrrr_ </a>
+                        <br>
+                        Demikian tampilan web Posttest 5 saya, wassalamualaikum warahmatullahi wabarakatuh
+                    </p>
+                </div>
+                <div id="kontak">
+                    <i class="fa fa-whatsapp"> 085845723207 </i>
+                    <i class="fa fa-instagram"> anharrrrrr_ </i>
+                    <i class="fa fa-envelope-o"> anharkhoirun@gmail.com </i>
+                    <i class="fa fa-github"> Anhar12 </i>
+                </div>
+                <p> @Copyright 2022 - anharrrslbw - Made with HTML, CSS, JS, & PHP </p>
+            </footer>
+
+        </div>
+    </div>
+    <script src="scriptidx.js"></script>
+</body>
+
+</html>
