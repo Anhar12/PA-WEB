39def2ea304169dad0b51a4cae9e4862a794a1ec
Anhar Khoirun Najib <92861249+Anhar12@users.noreply.github.com>
2022-11-10 23:25:57 +0800

    Update admin_list_barang.php

--- produk/admin_list_barang.php
+++ produk/admin_list_barang.php
@@ -13,7 +13,7 @@ if ($_SESSION["priv"] != "admin") {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="img/AZ.ico">
+  <link rel="icon" href="../assets/AZ.ico">
   <title>AnharZtore</title>
   <link rel="stylesheet" href="../style.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
