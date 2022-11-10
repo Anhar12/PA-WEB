3868dfc03bdcf93b86e4dede3527aa4d685ca1a8
Anhar Khoirun Najib <92861249+Anhar12@users.noreply.github.com>
2022-11-10 23:27:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require 'koneksi.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="img/AZ.png">
+    <link rel="icon" href="assets/AZ.ico">
     <title>AnharZtore</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
