306784970b6190fef431d7db0196befbe4703b8d
IAmGroots <hadiseptian.crb@gmail.com>
2022-11-12 22:10:07 +0800

    typo footer

--- produk/list_barang.php
+++ produk/list_barang.php
@@ -23,6 +23,7 @@ if (isset($_SESSION["username"])){
   <link rel="icon" href="../assets/AZ.ico">
   <title>AnharZtore</title>
   <link rel="stylesheet" href="../style.css">
+  <link rel="stylesheet" href="../style_footer.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
